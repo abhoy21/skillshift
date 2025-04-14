629bf19562cbfed0cc793e7c42173e1f32a552bb
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-04-14 21:08:45 +0530

    prisma prod fix

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -169,11 +169,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_wDeGVgbm5xB9@ep-lingering-glade-a5lt29sp-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },

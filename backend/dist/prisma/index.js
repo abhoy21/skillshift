24458e5051d59d63a99dd92b4ff954be168f4dd9
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-04-15 15:45:02 +0530

    prisma prod build fix try 4

--- backend/dist/prisma/index.js
+++ backend/dist/prisma/index.js
@@ -1,13 +1,8 @@
 "use strict";
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-const prismaClientSingleton = () => {
-    return new client_1.PrismaClient();
-};
-const prisma = (_a = globalThis.prisma) !== null && _a !== void 0 ? _a : prismaClientSingleton();
-exports.prisma = prisma;
-if (process.env.NODE_ENV !== "production") {
-    globalThis.prisma = prisma;
-}
+exports.prisma = new client_1.PrismaClient({
+    log: ["query"],
+    errorFormat: "pretty",
+});
